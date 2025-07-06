cd1fa04f7b8cf74500b8d52b87adb76f1040f065
khadyyade <khady0804@gmail.com>
2025-07-06 19:11:37 +0200

    feat(sync): add MD5 hash comparison for file changes

--- duosync.py
+++ duosync.py
@@ -1,6 +1,15 @@
 import os
 import shutil
 from pathlib import Path
+import argparse
+import hashlib
+
+def get_file_hash(file_path):
+    file_hash = hashlib.md5() #MD5 is an algorithm that generates a unique 128-bit hash number (32 hexadecimal characters) for any data
+    with open(file_path, "rb") as f:#MD5 only works with bytes, not with text
+        for block in iter(lambda: f.read(4096), b""): #It reads the file in 4KB blocks until there is nothing left to read
+            file_hash.update(block) #For every chunk, we update the hash number
+    return file_hash.hexdigest()
 
 def sync_dirs(src_path, dst_path):
     src = Path(src_path)
@@ -8,9 +17,11 @@ def sync_dirs(src_path, dst_path):
 
     if not src.exists():
        print("The source directory does not exist")
+       return
     else:
        if not dst.exists():
            print("The destination directory does not exist")
+           return
 
     files_copied = 0
 
@@ -31,10 +42,16 @@ def sync_dirs(src_path, dst_path):
         print("All files are up to date.")
 
 if __name__ == "__main__":
-    #Temporary paths for testing
-    source = "/home/jadi/Documentos/prueba1_DuoSync"
-    destination = "/mnt/windows/Users/khady/Documents/prueba_destino"
-    sync_dirs(source, destination)
+    parser = argparse.ArgumentParser(
+        description="DuoSync - Synchronize two folders by copying new or modified files."
+    )
+    parser.add_argument("source", help="Path to the source folder")
+    parser.add_argument("destination", help="Path to the destination folder")
+
+    args = parser.parse_args()
+
+    sync_dirs(args.source, args.destination)
+
 
 
 
